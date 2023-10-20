5cd9204b3d808a6053e3850e89d3ace678e81c84
Christian Häussler <c-n-i@web.de>
2023-10-20 20:49:43 +0200

    Skip latest Tag

--- build.sh
+++ build.sh
@@ -8,8 +8,8 @@ docker tag docker.io/cniweb/$image:$version ghcr.io/cniweb/$image:latest
 docker tag docker.io/cniweb/$image:$version quay.io/cniweb/$image:$version
 docker tag docker.io/cniweb/$image:$version quay.io/cniweb/$image:latest
 docker push docker.io/cniweb/$image:$version
-docker push docker.io/cniweb/$image:latest
+#docker push docker.io/cniweb/$image:latest
 docker push ghcr.io/cniweb/$image:$version
-docker push ghcr.io/cniweb/$image:latest
+#docker push ghcr.io/cniweb/$image:latest
 docker push quay.io/cniweb/$image:$version
-docker push quay.io/cniweb/$image:latest
+#docker push quay.io/cniweb/$image:latest
