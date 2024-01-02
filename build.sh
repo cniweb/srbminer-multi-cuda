b406c9293c13472ab525ecb9c2a22c4a7c330ebd
Christian Häussler <c-n-i@web.de>
2024-01-02 22:51:07 +0100

    Bump to version 2.4.4

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-version="2.4.3"
+version="2.4.4"
 image="srbminer-multi-cuda"
 docker build . --tag docker.io/cniweb/$image:$version
 docker tag docker.io/cniweb/$image:$version docker.io/cniweb/$image:latest
@@ -8,8 +8,8 @@ docker tag docker.io/cniweb/$image:$version ghcr.io/cniweb/$image:latest
 docker tag docker.io/cniweb/$image:$version quay.io/cniweb/$image:$version
 docker tag docker.io/cniweb/$image:$version quay.io/cniweb/$image:latest
 docker push docker.io/cniweb/$image:$version
-#docker push docker.io/cniweb/$image:latest
+docker push docker.io/cniweb/$image:latest
 docker push ghcr.io/cniweb/$image:$version
-#docker push ghcr.io/cniweb/$image:latest
+docker push ghcr.io/cniweb/$image:latest
 docker push quay.io/cniweb/$image:$version
-#docker push quay.io/cniweb/$image:latest
+docker push quay.io/cniweb/$image:latest
